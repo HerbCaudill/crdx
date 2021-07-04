065e09138fa08a80575a396a1d7588564d6ac733
HerbCaudill <herb@devresults.com>
2021-07-04 18:14:35 +0200

    initial

---
+++ src/util/testing/index.ts
@@ -0,0 +1,5 @@
+﻿export * from '@/util/testing/connectionHelpers'
+export * from '@/util/testing/joinTestChannel'
+export * from '@/util/testing/setup'
+export * from '@/util/testing/TestChannel'
+export * from '@/util/testing/Network'
---
+++ src/util/truncateHashes.ts
@@ -0,0 +1,16 @@
+﻿export function truncateHashes(arg: any) {
+  if (typeof arg === 'string') {
+    const str = arg as string
+    const hashRx = /(?:[A-Za-z0-9+/=]{32,100})?/g
+    return str.replace(hashRx, s => s.slice(0, 5))
+  } else if (typeof arg === 'object') {
+    const obj = {} as any
+    for (const prop in arg) {
+      const value = arg[prop]
+      obj[prop] = truncateHashes(value)
+    }
+    return obj
+  } else {
+    return arg
+  }
+}
