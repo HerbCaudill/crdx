908ead2ed72111beb3cbefd4e537316406f0c5e5
HerbCaudill <herb@devresults.com>
2021-09-16 15:50:30 +0200

    store/Store.ts:

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -53,7 +53,6 @@ export class Store<S, A extends Action, C = {}> extends EventEmitter {
   }
 
   public getState(): S {
-    if (this.isDispatching) throw new Error(`Can't call store.getState() while the reducer is executing. `)
     return this.state
   }
 
@@ -61,13 +60,22 @@ export class Store<S, A extends Action, C = {}> extends EventEmitter {
     return this.chain
   }
 
+  /** Dispatches an action to be added to the signature chain. This is the only way to trigger a
+   *  state change.
+   *
+   *  The `reducer` function provided when creating the store will be called with the current state
+   *  and the given `action`. Its return value will be considered the **next** state of the tree,
+   *  and any change listeners will be notified.
+   *
+   *  @param action A plain object representing what changed. It is a good idea to keep actions
+   *  serializable so you can record and replay user sessions. An action must have a `type` property
+   *  which may not be `undefined`. It is a good idea to use string constants for action types.
+   *
+   *  @returns For convenience, the same action object you dispatched.
+   */
   public dispatch(action: Optional<A, 'payload'>) {
-    if (this.isDispatching) throw new Error('Reducers may not dispatch actions.')
-
     // equip the action with an empty payload if it doesn't have one
     const actionWithPayload = { payload: undefined, ...action } as A
-
-    this.isDispatching = true
 
     // append this action as a new link to the chain
     this.chain = append({ chain: this.chain, action: actionWithPayload, user: this.user, context: this.context })
@@ -78,18 +86,24 @@ export class Store<S, A extends Action, C = {}> extends EventEmitter {
     // we don't need to pass the whole chain through the reducer, just the current state + the new head
     this.state = this.reducer(this.state, head)
 
-    this.isDispatching = false
+    // notify listeners
     this.emit('updated', { head: this.chain.head })
 
-    return this
+    return action
   }
 
+  /**
+   * Merges another chain (e.g. from a peer) with ours.
+   * @param theirChain
+   * @returns this `Store` instance
+   */
   public merge(theirChain: SignatureChain<A, C>) {
     this.chain = merge(this.chain, theirChain)
     this.updateState()
     return this
   }
 
+  /** Returns a serialized chain that can be used to rehydrate this store from storage. */
   public save() {
     return serialize(this.chain)
   }
@@ -111,7 +125,6 @@ export class Store<S, A extends Action, C = {}> extends EventEmitter {
   private validators?: ValidatorSet
 
   private state: S
-  private isDispatching = false
 
   private updateState() {
     const { chain, resolver, reducer } = this
@@ -125,6 +138,7 @@ export class Store<S, A extends Action, C = {}> extends EventEmitter {
     // Run the sequence through the reducer to calculate the current team state
     this.state = sequence.reduce(reducer, this.initialState)
 
+    // notify listeners
     this.emit('updated', { head: chain.head })
   }
 }
