3fbdd9035b11803156ab0ce1f6b0c1ffd68b372d
HerbCaudill <herb@devresults.com>
2021-10-13 13:32:10 +0200

    chain/append.ts: plural heads

--- src/chain/append.ts
+++ src/chain/append.ts
@@ -15,16 +15,15 @@ export const append = <A extends Action, C>({
   user: UserWithSecrets
   context?: C
 }): SignatureChain<A, C> => {
-  // chain to previous head
   const body = {
     ...action,
     user: redactUser(user),
     timestamp: new Date().getTime(),
     ...context,
-    ...(chain.head ? { prev: chain.head } : {}),
   } as LinkBody<A, C>
 
-  // if ('head' in chain) body.prev = chain.head
+  // chain to previous head(s). if none exists, this is the root node.
+  if (chain.head) body.prev = chain.head
 
   const { userName, keys } = user
   const hash = hashLink(body)
@@ -45,6 +44,6 @@ export const append = <A extends Action, C>({
 
   // return new chain
   const root = chain.root ?? hash // if no root was given, this was the first link
-  const head = hash
+  const head = [hash]
   return { root, head, links } as SignatureChain<A, C>
 }
