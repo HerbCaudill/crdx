8f56cebe296f60a58e87cf6b89dfb753237e75fa
HerbCaudill <herb@devresults.com>
2021-10-13 18:39:56 +0200

    _test/chain/append.test.ts: check for valid chain

--- src/_test/chain/append.test.ts
+++ src/_test/chain/append.test.ts
@@ -3,6 +3,7 @@ import { getHead } from '/chain/getHead'
 import { getRoot } from '/chain/getRoot'
 import { setup } from '/test/util/setup'
 import '/test/util/expect/toBeValid'
+import { validate } from '/validator'
 
 const { alice } = setup('alice')
 const defaultUser = alice
@@ -13,6 +14,9 @@ describe('chains', () => {
   test('append', () => {
     const chain1 = createChain({ user: defaultUser, name: 'a' })
     const chain2 = append({ chain: chain1, action: { type: 'FOO', payload: 'b' }, user: defaultUser })
+
+    expect(validate(chain2)).toBeValid()
+
     expect(getRoot(chain2)).toEqual(__({ body: __({ payload: __({ name: 'a' }) }) }))
     expect(getHead(chain2)).toEqual([__({ body: __({ payload: 'b' }) })])
   })
