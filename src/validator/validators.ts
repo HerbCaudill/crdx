df6e20bd0fc2998b573fa6cd04f82ce78588e148
HerbCaudill <herb@devresults.com>
2021-10-13 14:01:50 +0200

    validator/validators.ts: plural heads

--- src/validator/validators.ts
+++ src/validator/validators.ts
@@ -2,25 +2,23 @@
 import { hashLink } from '/chain/hashLink'
 import { isMergeLink, isRootLink } from '/chain/types'
 import { ROOT, VALID } from '/constants'
-import { memoize } from '/util'
+import { assert, memoize } from '/util'
 import { signatures } from '@herbcaudill/crypto'
 import { ValidationError, ValidatorSet } from './types'
 
 const _validators: ValidatorSet = {
-  /** Does the previous link referenced by this link exist?  */
+  /** Do the previous link(s) referenced by this link exist?  */
   validatePrev: (link, chain) => {
-    if (isRootLink(link)) {
-      // ROOT link
-      return VALID // nothing to validate on first link
-    } else {
-      const prevHashes = isMergeLink(link) ? link.body : [link.body.prev]
-      for (const hash of prevHashes) {
-        const prevLink = chain.links[hash]
-        if (prevLink === undefined)
-          return fail(`The link referenced by the hash in the \`prev\` property does not exist.`)
-      }
-      return VALID
-    }
+    // TODO: there are no merge links
+    assert(!isMergeLink(link))
+
+    if (isRootLink(link)) return VALID // nothing to validate on first link
+
+    for (const hash of link.body.prev)
+      if (!(hash in chain.links))
+        return fail(`The link referenced by the hash in the \`prev\` property does not exist.`)
+
+    return VALID
   },
 
   /** Does this link's hash check out? */
@@ -28,37 +26,36 @@ const _validators: ValidatorSet = {
     const { hash, body } = link
     const expected = hashLink(body)
     if (hash === expected) return VALID
-    else {
-      return fail(`The hash calculated for this link does not match.`, { link, hash, expected })
-    }
+    else return fail(`The hash calculated for this link does not match.`, { link, hash, expected })
   },
 
   /** If this is a root link, it should not have any predecessors, and should be the chain's root */
   validateRoot: (link, chain) => {
-    if (isMergeLink(link)) return VALID
+    // TODO: there are no merge links
+    assert(!isMergeLink(link))
+
     const hasNoPrevLink = !('prev' in link.body) || (link.body as any).prev === undefined
     const hasRootType = 'type' in link.body && link.body.type === ROOT
     const isTheChainRoot = getRoot(chain) === link
     // all should be true, or all should be false
-    if (hasNoPrevLink === isTheChainRoot && isTheChainRoot === hasRootType) {
-      return VALID
-    } else {
-      const message = hasRootType
-        ? // ROOT
-          hasNoPrevLink
-          ? `The ROOT link cannot have a predecessor (\`prev\` property)` // ROOT but has prev link
-          : `The ROOT link has to be the link referenced by the chain \`root\` property` // ROOT but isn't chain root
-        : // not ROOT
+    if (hasNoPrevLink === isTheChainRoot && isTheChainRoot === hasRootType) return VALID
+
+    const message = hasRootType
+      ? // ROOT
         hasNoPrevLink
-        ? `Non-ROOT links must have a predecessor (\`prev\` property)` // not ROOT but has no prev link
-        : `The link referenced by the chain \`root\` property must be a ROOT link` // not ROOT but is the chain root
-      return fail(message, { link, chain })
-    }
+        ? `The ROOT link cannot have a predecessor (\`prev\` property)` // ROOT but has prev link
+        : `The ROOT link has to be the link referenced by the chain \`root\` property` // ROOT but isn't chain root
+      : // not ROOT
+      hasNoPrevLink
+      ? `Non-ROOT links must have a predecessor (\`prev\` property)` // not ROOT but has no prev link
+      : `The link referenced by the chain \`root\` property must be a ROOT link` // not ROOT but is the chain root
+    return fail(message, { link, chain })
   },
 
   /** Does this link's signature check out? */
   validateSignatures: link => {
-    if (isMergeLink(link)) return VALID // merge links aren't signed
+    // TODO: there are no merge links
+    assert(!isMergeLink(link))
 
     const signedMessage = {
       payload: link.body,
