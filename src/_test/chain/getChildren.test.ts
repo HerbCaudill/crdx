bfaa3ad6eecb301ff64ca175694f565af2d5660d
HerbCaudill <herb@devresults.com>
2021-10-31 16:30:25 +0100

    _test/chain/getChildren.test.ts: create

---
+++ src/_test/chain/getChildren.test.ts
@@ -0,0 +1,20 @@
+import { buildComplexChain, findByPayload } from './utils'
+import { getChildren } from '/chain/getChildren'
+
+describe('getChildren', () => {
+  /*           
+                
+                          ┌─ e ─ g ─┐
+                ┌─ c ─ d ─┤         ├─ o ─┐
+         a ─ b ─┤         └─── f ───┤     ├─ n
+                ├──── h ──── i ─────┘     │ 
+                └───── j ─── k ── l ──────┘           
+    */
+  const chain = buildComplexChain()
+
+  test('root has 1 child', () => expect(getChildren(chain, chain.root)).toHaveLength(1))
+  test('b has 3 children', () => expect(getChildren(chain, findByPayload(chain, 'b').hash)).toHaveLength(3))
+  test('d has 2 children', () => expect(getChildren(chain, findByPayload(chain, 'd').hash)).toHaveLength(2))
+  test('e has 1 child', () => expect(getChildren(chain, findByPayload(chain, 'e').hash)).toHaveLength(1))
+  test('n has 0 children', () => expect(getChildren(chain, findByPayload(chain, 'n').hash)).toHaveLength(0))
+})
