0bfacb9cb41f9ff74b5c14cb2ed0bc336d17a37c
HerbCaudill <herb@devresults.com>
2021-10-31 16:30:23 +0100

    chain/getChildren.ts: create

---
+++ src/chain/getChildren.ts
@@ -0,0 +1,37 @@
+import { memoize } from 'lodash'
+import { Action, isRootLink, SignatureChain } from '/chain/types'
+import { Hash } from '/util'
+
+export const getChildren = <A extends Action, C>(chain: SignatureChain<A, C>, hash: Hash) => {
+  const childrenLookup = calculateChildren(chain)
+  return childrenLookup[hash] || []
+}
+
+/**
+ * Creates a lookup (by hash) containing the children of the corresponding link.
+ *  ```
+ * {
+ *    hash: [childHash, childHash, ...],
+ *    hash: [childHash, childHash, ...],
+ * }
+ * ```
+ */
+const calculateChildren = memoize(<A extends Action, C>(chain: SignatureChain<A, C>) => {
+  const childrenLookup = {} as Record<Hash, Hash[]>
+
+  // find the parents of each link, and add them to a dictionary lookup
+  for (const link of Object.values(chain.links)) {
+    // the root link has no parents
+    if (isRootLink(link)) continue
+
+    // every other link has one or more parents
+    const parents = link.body.prev
+    for (const parent of parents) {
+      // add this link's hash to each parent's list of children
+      const children = childrenLookup[parent] || []
+      children.push(link.hash)
+      childrenLookup[parent] = children
+    }
+  }
+  return childrenLookup
+})
