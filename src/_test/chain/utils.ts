f4c4d043f1418446cd5a45a8b19a474d5f5f4ccb
HerbCaudill <herb@devresults.com>
2021-11-07 12:58:20 +0100

    _test/chain/utils.ts:

--- src/_test/chain/utils.ts
+++ src/_test/chain/utils.ts
@@ -2,7 +2,6 @@
 import { createChain } from '/chain/createChain'
 import { merge } from '/chain/merge'
 import { Action, Link, LinkBody, SignatureChain, isRootLink } from '/chain/types'
-import clone from 'lodash/clone'
 import { setup } from '/test/util/setup'
 
 const { alice } = setup('alice')
@@ -18,112 +17,115 @@ export const findByPayload = (chain: SignatureChain<XAction, any>, payload: XAct
   return links.find(n => n.body.payload === payload) as Link<XAction, any>
 }
 
-/**
- * Returns this chain:
- *```
- *        ┌─ b
- *     a ─┤
- *        └─ c
- *```
- */
-export const buildSimpleChain = () => {
-  let root = createChain<XAction, any>({ user: alice, name: 'root' })
-  let a = appendLink(root, 'a')
+export const buildChain = (type: string) => {
+  let root = createChain<XAction>({ user: alice, name: 'root' })
+  switch (trim(type)) {
+    // one link
+    case 'a': {
+      let a = appendLink(root, 'a')
+      return a
+    }
 
-  let a1 = appendLink(clone(a), 'b')
-  let a2 = appendLink(clone(a), 'c')
+    // no branches
+    case trim(`a ─ b ─ c`): {
+      let a = appendLink(root, 'a')
+      let b = appendLink(a, 'b')
+      let c = appendLink(b, 'c')
+      return c
+    }
 
-  return merge(a1, a2)
-}
+    // simple
+    case trim(`
+            ┌─ b
+         a ─┤
+            └─ c
+      `): {
+      let a = appendLink(root, 'a')
+      let b = appendLink(a, 'b')
+      let c = appendLink(a, 'c')
+      return merge(b, c)
+    }
 
-/**
- * Returns this chain:
- *```
- *                      ┌─ e ─ g ─┐
- *            ┌─ c ─ d ─┤         ├─ o ─┐
- *     a ─ b ─┤         └─── f ───┤     ├─ n
- *            ├──── h ──── i ─────┘     │
- *            └───── j ─── k ── l ──────┘
- *
- *```
- */
-export const buildComplexChain = () => {
-  let root = createChain<XAction, any>({ user: alice, name: 'root' })
-  let a = appendLink(root, 'a')
-  let b = appendLink(a, 'b')
+    // complex
+    case trim(`
+                          ┌─ e ─ g ─┐
+                ┌─ c ─ d ─┤         ├─ o ─┐
+         a ─ b ─┤         └─── f ───┤     ├─ n
+                ├──── h ──── i ─────┘     │
+                └───── j ─── k ── l ──────┘
+      `): {
+      let a = appendLink(root, 'a')
+      let b = appendLink(a, 'b')
+      let c = appendLink(b, 'c')
+      let d = appendLink(c, 'd')
+      let e = appendLink(d, 'e')
+      let g = appendLink(e, 'g')
 
-  // 3 branches from b:
-  let b1 = clone(b)
-  let b2 = clone(b)
-  let b3 = clone(b)
+      let f = appendLink(d, 'f')
 
-  b1 = appendLink(b1, 'c')
-  b1 = appendLink(b1, 'd')
+      let h = appendLink(b, 'h')
+      let i = appendLink(h, 'i')
 
-  // 2 branches from d:
-  let d1 = clone(b1)
-  let d2 = clone(b1)
+      let j = appendLink(b, 'j')
+      let k = appendLink(j, 'k')
+      let l = appendLink(k, 'l')
 
-  d1 = appendLink(d1, 'e')
-  d1 = appendLink(d1, 'g')
+      let o = appendLink(merge(g, merge(f, i)), 'o')
 
-  d2 = appendLink(d2, 'f')
+      let n = appendLink(merge(o, l), 'n')
+      return n
+    }
 
-  b1 = merge(d1, d2) // *fg
+    // tricky
+    case trim(`
+                          ┌─── h ────┐
+                ┌─ c ─ e ─┤          ├─ k
+         a ─ b ─┤         └── i ─ j ─┘
+                └── d ────────┘
+      `): {
+      let a = appendLink(root, 'a')
+      let b = appendLink(a, 'b')
+      let c = appendLink(b, 'c')
+      let e = appendLink(c, 'e')
+      let h = appendLink(e, 'h')
 
-  b2 = appendLink(b2, 'h')
-  b2 = appendLink(b2, 'i')
+      let d = appendLink(b, 'd')
 
-  b1 = merge(b1, b2) // *i(fg)
+      let i = appendLink(merge(e, d), 'i')
+      let j = appendLink(i, 'j')
 
-  b1 = appendLink(b1, 'o')
+      let k = appendLink(merge(h, j), 'k')
+      return k
+    }
 
-  b3 = appendLink(b3, 'j')
-  b3 = appendLink(b3, 'k')
-  b3 = appendLink(b3, 'l')
+    // multiple heads
+    case trim(`
+                          ┌─ e ─ g ─┐
+                ┌─ c ─ d ─┤         ├─ o 
+         a ─ b ─┤         └─── f ───┘     
+                ├─ h ─ i  
+                └─ j 
+      `): {
+      let a = appendLink(root, 'a')
+      let b = appendLink(a, 'b')
+      let c = appendLink(b, 'c')
+      let d = appendLink(c, 'd')
+      let e = appendLink(d, 'e')
+      let g = appendLink(e, 'g')
+      let f = appendLink(d, 'f')
+      let o = appendLink(merge(g, f), 'o')
 
-  a = merge(b1, b3) // *ol
+      let h = appendLink(b, 'h')
+      let i = appendLink(h, 'i')
 
-  a = appendLink(a, 'n')
+      let j = appendLink(b, 'j')
 
-  return a
-}
+      return merge(o, merge(i, j))
+    }
 
-/**
- * Returns this chain:
- *```
- *                      ┌─ h ────────┐
- *            ┌─ c ─ e ─┤            ├─ k
- *     a ─ b ─┤         └──┐         │
- *            │            ├─ g ─ j ─┘
- *            └── d ───────┘
- *```
- */
-export const buildTrickyChain = () => {
-  let root = createChain<XAction, any>({ user: alice, name: 'root' })
-  let a = appendLink(root, 'a')
-  let b = appendLink(a, 'b')
-
-  // 2 branches from b:
-  let b1 = clone(b)
-  let b2 = clone(b)
-
-  b1 = appendLink(b1, 'c')
-  b1 = appendLink(b1, 'e')
-
-  b2 = appendLink(b2, 'd')
-
-  let e1 = merge(b1, b2)
-
-  e1 = appendLink(e1, 'g')
-  e1 = appendLink(e1, 'j')
-
-  let e2 = appendLink(b2, 'h')
-
-  a = merge(e1, e2)
-  a = appendLink(a, 'k')
-
-  return a
+    default:
+      throw new Error('unknown chain')
+  }
 }
 
 export const getHashes = (chain: SignatureChain<any, any>) => Object.keys(chain.links)
@@ -136,3 +138,5 @@ export type XLink = Link<XAction, {}>
 
 const appendLink = (chain: SignatureChain<XAction, any>, payload: string) =>
   append({ chain, action: { type: 'X', payload } as XAction, user: alice })
+
+const trim = (s: string) => s.replace(/\s*/g, '')
