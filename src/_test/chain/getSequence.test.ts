0d225ace8677b5d38ef2b53d65106bc659330acc
HerbCaudill <herb@devresults.com>
2021-11-08 14:55:05 +0100

    _test/chain/getSequence.test.ts:

--- src/_test/chain/getSequence.test.ts
+++ src/_test/chain/getSequence.test.ts
@@ -1,46 +1,63 @@
-import { randomKey } from '@herbcaudill/crypto'
-import { append, arbitraryDeterministicSort, createChain, getSequence, Resolver, Sequence } from '/chain'
-import { buildChain, findByPayload, getPayloads, XAction, XLink } from '/test/chain/utils'
-import { setup } from '/test/util/setup'
+import { getConcurrentLinks, getSequence, Resolver } from '/chain'
+import { buildChain, getPayloads, XAction } from '/test/chain/utils'
+import { Hash } from '/util'
 
-const { alice } = setup('alice')
+/**
+ * Custom logic:
+ * 1. if `e` and `f` are concurrent, `e` is invalid
+ * 2. if `h` and `i` are concurrent, both are invalid
+ * 3. `j` comes first, otherwise sort alphabetically
+ */
+const resolver: Resolver<XAction, any> = chain => {
+  const invalid: Record<Hash, boolean> = {}
+  for (const link of Object.values(chain.links)) {
+    const concurrentLinks = getConcurrentLinks(chain, link)
+    for (const concurrentLink of concurrentLinks) {
+      // rule 1
+      if (link.body.payload === 'f' && concurrentLink.body.payload === 'e') {
+        invalid[concurrentLink.hash] = true
+      }
+      // rule 2
+      if (link.body.payload === 'h' && concurrentLink.body.payload === 'i') {
+        invalid[link.hash] = true
+        invalid[concurrentLink.hash] = true
+      }
+    }
+  }
+  return {
+    sort: (_a, _b) => {
+      const a = _a.body.payload
+      const b = _b.body.payload
 
-const byPayload = (a: Sequence<XAction, any>, b: Sequence<XAction, any>) => {
-  const [aPayload, bPayload] = [a, b].map(branch => getPayloads(branch))
-  return aPayload < bPayload ? -1 : aPayload > bPayload ? 1 : 0
-}
+      // rule 3
+      if (a === 'j') return -1
+      if (b === 'j') return 1
 
-const alphabeticalResolver: Resolver<XAction, any> = branches => {
-  const [a, b] = branches.sort(byPayload)
-  return a.concat(b)
-}
-
-const randomResolver: Resolver<any, any> = ([a, b]) => {
-  // change the hash key on each run, to ensure our tests aren't bound to one arbitrary sort
-  const hashKey = randomKey()
-  const [_a, _b] = [a, b].sort(arbitraryDeterministicSort(hashKey))
-  return _a.concat(_b)
+      if (a < b) return -1
+      if (a > b) return 1
+      return 0
+    },
+    filter: link => {
+      return !invalid[link.hash]
+    },
+  }
 }
 
 describe('chains', () => {
   describe('getSequence', () => {
-    test('upon creation', () => {
-      var chain = createChain({ user: alice, name: 'root' })
-      chain = append({ chain, action: { type: 'X', payload: 'a' }, user: alice })
-      const sequence = getSequence({ chain, resolver: randomResolver })
-      expect(getPayloads(sequence)).toEqual(['a'])
+    test('one link', () => {
+      const chain = buildChain('a')
+      const sequence = getSequence(chain)
+      const payloads = getPayloads(sequence)
+      expect(payloads).toEqual('a')
     })
 
     test('no branches', () => {
-      var chain = createChain({ user: alice, name: 'root' })
-      chain = append({ chain, action: { type: 'X', payload: 'a' }, user: alice })
-      chain = append({ chain, action: { type: 'X', payload: 'b' }, user: alice })
-      chain = append({ chain, action: { type: 'X', payload: 'c' }, user: alice })
-      const sequence = getSequence({ chain, resolver: randomResolver })
+      const chain = buildChain(`a ─ b ─ c`)
+      const sequence = getSequence(chain)
+      const payloads = getPayloads(sequence)
 
-      const expected = 'a b c'
-
-      expect(getPayloads(sequence)).toEqual(expected)
+      expect(payloads).toEqual('abc')
     })
 
     test('simple chain', () => {
@@ -49,11 +66,13 @@ describe('chains', () => {
        a ─┤
           └─ c
       `)
-      const sequence = getSequence({ chain, resolver: alphabeticalResolver })
-      expect(getPayloads(sequence)).toEqual('abc')
+
+      const sequence = getSequence(chain, resolver)
+      const payloads = getPayloads(sequence)
+      expect(payloads).toEqual('abc')
     })
 
-    describe('complex chain', () => {
+    test('complex chain', () => {
       const chain = buildChain(`
                           ┌─ e ─ g ─┐
                 ┌─ c ─ d ─┤         ├─ o ─┐
@@ -61,95 +80,43 @@ describe('chains', () => {
                 ├──── h ──── i ─────┘     │ 
                 └───── j ─── k ── l ──────┘           
       `)
+      const sequence = getSequence(chain, resolver)
+      const payloads = getPayloads(sequence)
+      // without resolver we'd get `abcdegfhiojkln`
+      // `e` is removed (rule 1)
+      // `jkl` comes before `c` (rule 3)
+      expect(payloads).toEqual('abjklcdgfhion')
+    })
 
-      test('full sequence', () => {
-        const sequence = getSequence({ chain, resolver: alphabeticalResolver })
-        expect(getPayloads(sequence)).toEqual('abcdegfhiojkln')
-      })
+    test('tricky chain', () => {
+      const chain = buildChain(`
+                          ┌─── h ────┐
+                ┌─ c ─ e ─┤          ├─ k
+         a ─ b ─┤         └── i ─ j ─┘
+                └── d ────────┘
+      `)
 
-      test('root', () => {
-        const b = findByPayload(chain, 'b')
-        const sequence = getSequence<any, any>({ chain, root: b, resolver: randomResolver })
+      const sequence = getSequence(chain, resolver)
+      const payloads = getPayloads(sequence)
+      // without resolver we'd get `abcehdijk`
+      // `h` and `i` are removed (rule 2)
+      expect(payloads).toEqual('abcedjk')
+    })
 
-        const expected = [
-          'b jkl cdfeg hi on',
-          'b jkl cdegf hi on',
-
-          'b jkl hi cdfeg on',
-          'b jkl hi cdegf on',
-
-          'b hi cdfeg jkl on',
-          'b hi cdegf jkl on',
-
-          'b hi cdfeg o jkl n',
-          'b hi cdegf o jkl n',
-
-          'b cdfeg hi o jkl n',
-          'b cdegf hi o jkl n',
-
-          'b cdfeg hi jkl on',
-          'b cdegf hi jkl on',
-        ].map(trim)
-
-        expect(expected).toContainEqual(getPayloads(sequence))
-      })
-
-      test('head', () => {
-        const d = findByPayload(chain, 'd')
-        const sequence = getSequence<any, any>({ chain, head: [d], resolver: randomResolver })
-
-        const expected = 'abcd'
-
-        expect(getPayloads(sequence)).toEqual(expected)
-      })
-
-      test('root & head', () => {
-        const j = findByPayload(chain, 'j')
-        const l = findByPayload(chain, 'l')
-        const sequence = getSequence<any, any>({ chain, root: j, head: [l], resolver: randomResolver })
-
-        const expected = 'j k l'
-
-        expect(getPayloads(sequence)).toEqual(expected)
-      })
-
-      test('root within a branch', () => {
-        const c = findByPayload(chain, 'c')
-        const sequence = getSequence<any, any>({ chain, root: c, resolver: randomResolver })
-
-        expect(['cdegfon', 'cdfegon']).toContainEqual(getPayloads(sequence))
-      })
-
-      test('custom resolver', () => {
-        const resolver: Resolver<XAction, any> = ([_a, _b]) => {
-          // inclusion rules: `e`s are omitted
-
-          const eFilter = (n: XLink) => n.body.payload !== 'e'
-          const [a, b] = [_a.filter(eFilter), _b.filter(eFilter)]
-
-          // sequence rules: `i`s go first, otherwise alphabetical
-
-          const alpha = (a: XLink, b: XLink) => (a.body.payload! > b.body.payload! ? 1 : -1)
-          const merged = a.concat(b).sort(alpha)
-
-          const isI = (n: XLink) => n.body.payload === 'i'
-          const Is = merged.filter(n => isI(n))
-          const notIs = merged.filter(n => !isI(n))
-
-          const sequencedBranches = Is.concat(notIs)
-
-          return sequencedBranches
-        }
-
-        const sequence = getSequence({ chain, resolver })
-
-        // note that `i` comes first in the merged portion, and `e` is omitted
-        const expected = 'abicdfghjklon'
-
-        expect(getPayloads(sequence)).toEqual(expected)
-      })
+    test('multiple heads', () => {
+      const chain = buildChain(`
+                          ┌─ e ─ g ─┐
+                ┌─ c ─ d ─┤         ├─ o 
+         a ─ b ─┤         └─── f ───┘     
+                ├─ h ─ i  
+                └─ j 
+      `)
+      const sequence = getSequence(chain, resolver)
+      const payloads = getPayloads(sequence)
+      // without resolver we'd get `abhijcdfego`
+      // `e` is removed (rule 1)
+      // `j` comes before `c` (rule 3)
+      expect(payloads).toEqual('abjcdgfohi')
     })
   })
 })
-
-const trim = (s: string) => s.replace(/\s*/g, '')
