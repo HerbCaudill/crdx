7e3e23fb005212b658cd27f505407533b002a25b
HerbCaudill <herb@devresults.com>
2021-11-18 17:05:12 +0100

    chain/hashLink.ts:

--- src/chain/hashLink.ts
+++ src/chain/hashLink.ts
@@ -1,8 +1,9 @@
 import { hash } from '@herbcaudill/crypto'
+import { Action } from './types'
 import { HashPurpose } from '/constants'
 
 const { LINK_TO_PREVIOUS } = HashPurpose
 
-export const hashLink = (body: any) => {
+export const hashLink = <A extends Action>(body: A) => {
   return hash(LINK_TO_PREVIOUS, body)
 }
