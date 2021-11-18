ed3d8135662c50aafe8d947dbb2383d7ba16cfab
HerbCaudill <herb@devresults.com>
2021-11-18 15:22:17 +0100

    chain/predecessors.ts: reorder

--- src/chain/predecessors.ts
+++ src/chain/predecessors.ts
@@ -3,6 +3,9 @@ import { memoize } from '/util'
 import uniq from 'lodash/uniq'
 import { getLink } from './chain'
 
+export const getParents = <A extends Action, C>(chain: SignatureChain<A, C>, link: Link<A, C>) =>
+  link.body.prev.map(hash => getLink(chain, hash))
+
 export const getPredecessorHashes = memoize(
   <A extends Action, C>(chain: SignatureChain<A, C>, hash: string): string[] => {
     if (!(hash in chain.links)) return []
@@ -12,25 +15,14 @@ export const getPredecessorHashes = memoize(
   }
 )
 
+/** Returns the set of predecessors of `link` (not including `link`) */
+export const getPredecessors = <A extends Action, C>(chain: SignatureChain<A, C>, link: Link<A, C>): Link<A, C>[] =>
+  getPredecessorHashes(chain, link.hash)
+    .map(h => chain.links[h])
+    .filter(link => link !== undefined)
+
 export const isPredecessorHash = <A extends Action, C>(chain: SignatureChain<A, C>, a: string, b: string) =>
   getPredecessorHashes(chain, b).includes(a)
-
-export const getCommonPredecessorHash = memoize(
-  <A extends Action, C>(chain: SignatureChain<A, C>, a: string, b: string) => {
-    if (a === b) return a
-
-    // does one precede the other?
-    if (isPredecessorHash(chain, a, b)) return a
-    if (isPredecessorHash(chain, b, a)) return b
-
-    const aPredecessors = getPredecessorHashes(chain, a)
-    const bPredecessors = getPredecessorHashes(chain, b)
-    return aPredecessors.find(link => bPredecessors.includes(link))
-  }
-)
-
-export const getParents = <A extends Action, C>(chain: SignatureChain<A, C>, link: Link<A, C>) =>
-  link.body.prev.map(hash => getLink(chain, hash))
 
 /** Returns true if `a` is a predecessor of `b` */
 export const isPredecessor = <A extends Action, C>(
@@ -47,11 +39,19 @@ export const isPredecessor = <A extends Action, C>(
   )
 }
 
-/** Returns the set of predecessors of `link` (not including `link`) */
-export const getPredecessors = <A extends Action, C>(chain: SignatureChain<A, C>, link: Link<A, C>): Link<A, C>[] =>
-  getPredecessorHashes(chain, link.hash)
-    .map(h => chain.links[h])
-    .filter(link => link !== undefined)
+export const getCommonPredecessorHash = memoize(
+  <A extends Action, C>(chain: SignatureChain<A, C>, a: string, b: string) => {
+    if (a === b) return a
+
+    // does one precede the other?
+    if (isPredecessorHash(chain, a, b)) return a
+    if (isPredecessorHash(chain, b, a)) return b
+
+    const aPredecessors = getPredecessorHashes(chain, a)
+    const bPredecessors = getPredecessorHashes(chain, b)
+    return aPredecessors.find(link => bPredecessors.includes(link))! // guaranteed to exist
+  }
+)
 
 export const getCommonPredecessor = <A extends Action, C>(
   chain: SignatureChain<A, C>,
@@ -61,7 +61,6 @@ export const getCommonPredecessor = <A extends Action, C>(
   if (links.length === 2) {
     const [a, b] = links
     const hash = getCommonPredecessorHash(chain, a.hash, b.hash)
-    if (!hash) throw new Error('no common predecessor was found')
     return getLink(chain, hash)
   }
   return links.reduce((result, link) => getCommonPredecessor(chain, [result, link]), links[0])
