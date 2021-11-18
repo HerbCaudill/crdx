91171ab621063de232f2c02611df55a6b84e8ce6
HerbCaudill <herb@devresults.com>
2021-11-18 15:19:35 +0100

    util/assert.ts: ignore coverage

--- src/util/assert.ts
+++ src/util/assert.ts
@@ -1,4 +1,6 @@
-﻿export function assert(value: boolean, message?: string): asserts value
+﻿// ignore file coverage
+
+export function assert(value: boolean, message?: string): asserts value
 export function assert<T>(value: T | null | undefined, message?: string): asserts value is T
 export function assert(value: any, message: string = 'Assertion failed') {
   if (value === false || value === null || typeof value === 'undefined') {
