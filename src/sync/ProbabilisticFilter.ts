19d2bb3e9dd4bd710f5ebb9eccd64c461b275c69
HerbCaudill <herb@devresults.com>
2021-11-21 16:15:42 +0100

    sync/ProbabilisticFilter.ts:

--- src/sync/ProbabilisticFilter.ts
+++ src/sync/ProbabilisticFilter.ts
@@ -1,27 +1,16 @@
-import { hash } from '@herbcaudill/crypto'
 import { Hash } from '/util'
 
 export abstract class ProbabilisticFilter {
   constructor() {}
 
-  add(values: string[]) {
-    const hashes = values.map((value: string) => makeHash(value))
-    this.addHashes(hashes)
-    return this
-  }
+  abstract add(values: string[]): this
 
-  abstract addHashes(hashes: Hash[]): ProbabilisticFilter
+  abstract addHashes(hashes: Hash[]): this
 
-  has(value: string) {
-    return this.hasHash(makeHash(value))
-  }
+  abstract has(value: string): boolean
 
   abstract hasHash(hash: Hash): boolean
 
   abstract save(): Uint8Array
   abstract load(encodedValue: Uint8Array): ProbabilisticFilter
-}
-
-export const makeHash = (s: string) => {
-  return hash('ProbabilisticFilter', s)
 }
