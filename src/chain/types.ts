3209157fc80bbf164cf1a1e17fc46024ecb58939
HerbCaudill <herb@devresults.com>
2022-03-06 18:45:39 +0100

    add encrypted link & chain types

--- src/chain/types.ts
+++ src/chain/types.ts
@@ -1,25 +1,33 @@
 ﻿import { User } from '/user'
-import { Base58, Hash, UnixTimestamp } from '/util/types'
+import { Base58, Encrypted, Hash, Key, UnixTimestamp } from '/util/types'
 
 /**
- * A signature chain is an acyclic directed graph of links. Each link is **cryptographically
- * signed** by the author, and includes a **hash of the parent link**.
+ * A signature chain is an acyclic directed graph of links. Each link is **asymmetrically encrypted
+ * and authenticated** by the author, and includes **hashes of all known heads** at the time of
+ * authoring.
  *
  * This means that the chain is **append-only**: Existing nodes can’t be modified, reordered, or
- * removed without causing the hash and signature checks to fail.
+ * removed without causing the hash and authentication checks to fail.
  *
  * A signature chain is just data and can be stored as JSON. It consists of a hash table of the
  * links themselves, plus a pointer to the **root** (the “founding” link added when the chain was
- * created) and the **head** (the most recent link we know about).
+ * created) and the **head** (the most recent link(s) we know about).
  *
- * The `SignatureChain` interface takes two parameters:
+ * The `EncryptedSignatureChain` interface takes two parameters:
  *
- * - `A` is the Action type — typically a union of various `type` labels (e.g. 'ADD_CONTACT')
- *   along with the interface of the payload associated with each one.
- * - `C` is the Context interface — by default this is an empty interface, but might contain information
- *   about the context in which a link is added (e.g. a device ID, or the version of the application)
+ * - `A` is the Action type — typically a union of various `type` labels (e.g. 'ADD_CONTACT') along
+ *   with the interface of the payload associated with each one.
+ * - `C` is the Context interface — by default this is an empty interface, but might contain
+ *   information about the context in which a link is added (e.g. a device ID, or the version of the
+ *   application)
+ *
+ * The `EncryptedSignatureChain` can live in public. Each link is asymmetrically encrypted using the
+ * author's secret key and the team public key at time of authoring.
  */
-export interface SignatureChain<A extends Action, C> {
+export interface EncryptedSignatureChain<A extends Action, C> {
+  // TODO: version the chain?
+  // version: 3.0.0
+
   /** Hash of the root link (the "founding" link added when the chain was created) */
   root: Hash
 
@@ -27,7 +35,27 @@ export interface SignatureChain<A extends Action, C> {
   head: Hash[]
 
   /** Hash table of all the links we know about */
+  encryptedLinks?: Record<Hash, EncryptedLink<A, C>>
+}
+
+/**
+ * The `SignatureChain` interface adds the decrypted links, and is for local manipulation by the
+ * application.
+ */
+export interface SignatureChain<A extends Action, C> extends EncryptedSignatureChain<A, C> {
+  /** Decrypted links */
   links: Record<Hash, Link<A, C>>
+}
+
+export type EncryptedLink<A extends Action, C> = {
+  /** Public key of the author of the link, at the time of authoring. */
+  authorPublicKey: Base58
+
+  /**
+   * The body of the link, encrypted asymmetrically with authentication (using libsodium's
+   * `crypto_box`) using the author's SK and the team's PK.
+   */
+  encryptedBody: Encrypted<LinkBody<A, C>>
 }
 
 /** A link consists of a body, as well as a hash and a signature calculated from the body. */
@@ -35,8 +63,10 @@ export type Link<A extends Action, C> = {
   /** Hash of the body */
   hash: Hash
 
-  /** The part of the link that is signed & hashed */
+  /** The part of the link that is encrypted */
   body: LinkBody<A, C>
+
+  // TODO: remove signature block once it's not used
 
   /** The signature block (signature, name, and key) */
   signed: {
@@ -53,13 +83,15 @@ export type Link<A extends Action, C> = {
   isInvalid?: boolean
 }
 
+/** The root action's payload is defined by the application. */
 export interface RootAction {
   type: 'ROOT'
   payload: any
 }
 
-/** An `Action` is analogous to a Redux action: it has a string label (e.g. 'ADD_USER' or 'INCREMENT')
- * and a payload that can contain anything.
+/**
+ * An `Action` is analogous to a Redux action: it has a string label (e.g. 'ADD_USER' or
+ * 'INCREMENT') and a payload that can contain anything.
  */
 export type Action =
   | RootAction
