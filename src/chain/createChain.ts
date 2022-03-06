73d6b63c9ebdf7c1f7681ea7e4eefdcc7c840daa
HerbCaudill <herb@devresults.com>
2022-03-06 18:55:44 +0100

    chain/createChain.ts: add encryptedLinks

--- src/chain/createChain.ts
+++ src/chain/createChain.ts
@@ -7,6 +7,7 @@ import { UserWithSecrets } from '/user'
 export const EMPTY_CHAIN = {
   root: undefined,
   head: undefined,
+  encryptedLinks: {},
   links: {},
 }
 
@@ -23,7 +24,8 @@ export const createChain = <A extends Action, C = {}>({
   /** Unique identifier for the chain. If none is provided, a random one will be generated. */
   id?: string
 
-  /** Human facing name of the chain (e.g. document name, team name, etc). If none is provided, the `id` will be used. */
+  /** Human facing name of the chain (e.g. document name, team name, etc). This should be unique
+   * within the application's namespace. If none is provided, the `id` will be used. */
   name?: string
 
   /** Object containing information to be added to the ROOT link. */
