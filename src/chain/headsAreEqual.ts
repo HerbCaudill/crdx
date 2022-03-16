b3594cb6c88240a7974171db248a7fbf3de207ae
HerbCaudill <herb@devresults.com>
2022-03-16 14:54:40 +0100

    chain/headsAreEqual.ts: accept undefined inputs

--- src/chain/headsAreEqual.ts
+++ src/chain/headsAreEqual.ts
@@ -1,6 +1,7 @@
 import { Hash } from '/util'
 
-export const headsAreEqual = (a: Hash[], b: Hash[]) => {
+export const headsAreEqual = (a: Hash[] | undefined, b: Hash[] | undefined) => {
+  if (a === undefined || b === undefined) return false
   if (a.length !== b.length) return false
 
   a.sort()
