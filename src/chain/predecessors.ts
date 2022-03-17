871d64cc249fa3bec2adf0f97c4d40491d24b661
HerbCaudill <herb@devresults.com>
2022-03-17 17:03:56 +0100

    chain/predecessors.ts: clean up types

--- src/chain/predecessors.ts
+++ src/chain/predecessors.ts
@@ -1,15 +1,13 @@
 ﻿import { Action, Link, SignatureChain } from './types'
-import { memoize } from '/util'
+import { Hash, memoize } from '/util'
 import uniq from 'lodash/uniq'
 import { getLink } from './chain'
 
-export const getPredecessorHashes = memoize(
-  <A extends Action, C>(chain: SignatureChain<A, C>, hash: string): string[] => {
-    const parents = getLink(chain, hash).body.prev
-    const predecessors = parents.flatMap(parent => getPredecessorHashes(chain, parent))
-    return uniq(parents.concat(predecessors))
-  }
-)
+export const getPredecessorHashes = memoize((chain: SignatureChain<any, any>, hash: Hash): Hash[] => {
+  const parents: Hash[] = getLink(chain, hash)?.body.prev || []
+  const predecessors = parents.flatMap(parent => getPredecessorHashes(chain, parent))
+  return uniq(parents.concat(predecessors))
+})
 
 /** Returns the set of predecessors of `link` (not including `link`) */
 export const getPredecessors = <A extends Action, C>(chain: SignatureChain<A, C>, link: Link<A, C>): Link<A, C>[] =>
@@ -18,15 +16,11 @@ export const getPredecessors = <A extends Action, C>(chain: SignatureChain<A, C>
     .filter(link => link !== undefined)
 
 /** Returns true if `a` is a predecessor of `b` */
-export const isPredecessorHash = <A extends Action, C>(chain: SignatureChain<A, C>, a: string, b: string) =>
+export const isPredecessorHash = (chain: SignatureChain<any, any>, a: string, b: string) =>
   getPredecessorHashes(chain, b).includes(a)
 
 /** Returns true if `a` is a predecessor of `b` */
-export const isPredecessor = <A extends Action, C>(
-  chain: SignatureChain<A, C>,
-  a: Link<A, C>,
-  b: Link<A, C>
-): boolean => {
+export const isPredecessor = (chain: SignatureChain<any, any>, a: Link<any, any>, b: Link<any, any>) => {
   return (
     a !== undefined &&
     b !== undefined &&
