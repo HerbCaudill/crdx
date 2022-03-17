af964c2e40aa1482cf961f5c267e75c92f42bef4
HerbCaudill <herb@devresults.com>
2022-03-17 17:04:11 +0100

    sync/receiveMessage.ts: refactor

--- src/sync/receiveMessage.ts
+++ src/sync/receiveMessage.ts
@@ -1,9 +1,8 @@
 import { SyncMessage, SyncState } from './types'
-import { Action, LinkMap, merge, SignatureChain } from '/chain'
+import { Action, merge, SignatureChain } from '/chain'
 import { decryptChain } from '/chain/decrypt'
-import { getLinkMap, isComplete } from '../chain/linkMap'
 import { KeysetWithSecrets } from '/keyset'
-import { assert, Hash, truncateHashes } from '/util'
+import { assert } from '/util'
 import { validate } from '/validator'
 
 /**
@@ -19,7 +18,7 @@ export const receiveMessage = <A extends Action, C>(
   chain: SignatureChain<A, C>,
 
   /** Our sync state with this peer */
-  state: SyncState,
+  prevState: SyncState,
 
   /** The sync message they've just sent */
   message: SyncMessage<A, C>,
@@ -31,77 +30,46 @@ export const receiveMessage = <A extends Action, C>(
   // This should never happen, but just as a sanity check
   assert(chain.root === their.root, `Can't sync chains with different roots`)
 
-  state.their.head = their.head
-  state.their.reportedError = their.error
-
-  // store the new links in state
-  state.their.links = { ...state.their.links, ...their.links }
-
-  // merge this set of recent hashes with any they've sent previously
-  const theirLinkMap = {
-    ...state.their.linkMap,
-    ...their.linkMap,
+  const state: SyncState = {
+    ...prevState,
+    their: {
+      head: their.head,
+      need: their.need || [],
+      links: { ...prevState.their.links, ...their.links },
+      linkMap: { ...prevState.their.linkMap, ...their.linkMap },
+      reportedError: their.error,
+    },
   }
 
-  state.their.need = their.need || []
+  // if we've received links from them, try to reconstruct their chain and merge
+  if (Object.keys(state.their.links).length) {
+    // reconstruct their chain
+    const head = their.head
+    const encryptedLinks = { ...chain.encryptedLinks, ...state.their.links }
+    const encryptedChain = { ...chain, head, encryptedLinks }
+    const theirChain = decryptChain(encryptedChain, chainKeys)
 
-  // if they've sent new links and we have a full picture of their chain, try to merge it with ours
+    // merge with our chain
+    const mergedChain = merge(chain, theirChain)
 
-  const pendingHashes = Object.keys(state.their.links)
-  if (pendingHashes.length) {
-    // make a dependency map of the pending links
-    const mapOfPendingLinks: LinkMap = pendingHashes.reduce(
-      (linkMap, hash) => ({ ...linkMap, [hash]: theirLinkMap[hash] }),
-      {}
-    )
+    // check the integrity of the merged chain
+    const validation = validate(mergedChain)
 
-    // we'll combine that with a dependency map of our own chain
-    const mapOfOurChain: LinkMap = getLinkMap({ chain })
+    if (validation.isValid) {
+      chain = mergedChain
+    } else {
+      // We only get here if we've received bad links from them — maliciously, or not. The
+      // application should monitor `failedSyncCount` and decide not to trust them if it's too high.
+      state.failedSyncCount += 1
 
-    // are there any hashes we don't know about?
-    const weHaveTheirFullChain = isComplete({ ...mapOfOurChain, ...mapOfPendingLinks })
-
-    if (weHaveTheirFullChain) {
-      // there are no gaps — we can reconstruct their chain
-      const theirChain = {
-        ...chain,
-        head: state.their.head,
-        encryptedLinks: {
-          ...chain.encryptedLinks,
-          ...state.their.links,
-        },
-      }
-
-      // decrypt encrypted links
-      const theirDecryptedChain = decryptChain(theirChain, chainKeys)
-
-      // merge with our chain
-      const mergedChain = merge(chain, theirDecryptedChain)
-
-      // check the integrity of the merged chain
-      const validation = validate(mergedChain)
-
-      // TODO: should the application should have a hook for providing a custom validator set here,
-      // so that the merge fails e.g. if the author's public keys don't match up?
-
-      if (validation.isValid) {
-        chain = mergedChain
-      } else {
-        // We only get here because we've received bad links from them — maliciously, or not. The
-        // application should monitor `failedSyncCount` and decide not to trust them if it's too high.
-        state.failedSyncCount += 1
-
-        // Record the error so we can surface it in generateMessage
-        state.our.reportedError = validation.error
-      }
-
-      // either way, we can discard all pending links
-      state.their.links = {}
-      state.their.linkMap = {}
+      // Record the error so we can surface it in generateMessage
+      state.our.reportedError = validation.error
     }
-  }
 
-  state.their.linkMap = theirLinkMap
+    // either way, we can discard all pending links
+    state.their.links = {}
+    state.their.linkMap = {}
+  }
 
   return [chain, state]
 }
