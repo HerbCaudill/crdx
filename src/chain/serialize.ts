807438e2d5b105e66e0a2b30be0be334cf7e06ce
HerbCaudill <herb@devresults.com>
2022-03-30 14:55:31 +0200

    chain/serialize.ts: build in encryption & decryption

--- src/chain/serialize.ts
+++ src/chain/serialize.ts
@@ -1,9 +1,14 @@
-import { Action, HashGraph } from './types'
+import { decryptChain } from './decrypt'
+import { Action, EncryptedHashGraph, HashGraph } from './types'
+import { KeysetWithSecrets } from '/keyset'
 
 export const serialize = <A extends Action, C>(chain: HashGraph<A, C>) => {
-  return JSON.stringify(chain)
+  const { root, head, encryptedLinks } = chain
+  const encryptedChain = { root, head, encryptedLinks } as EncryptedHashGraph
+  return JSON.stringify(encryptedChain)
 }
 
-export const deserialize = <A extends Action, C>(serialized: string): HashGraph<A, C> => {
-  return JSON.parse(serialized) as HashGraph<A, C>
+export const deserialize = <A extends Action, C>(serialized: string, chainKeys: KeysetWithSecrets): HashGraph<A, C> => {
+  const encryptedChain = JSON.parse(serialized) as EncryptedHashGraph
+  return decryptChain(encryptedChain, chainKeys)
 }
