5735692d6f349c18baf904e92d11f44fa1180dcd
HerbCaudill <herb@devresults.com>
2022-03-30 14:54:58 +0200

    chain/decrypt.ts: accept partially decrypted graph

--- src/chain/decrypt.ts
+++ src/chain/decrypt.ts
@@ -1,6 +1,6 @@
 import { asymmetric } from '@herbcaudill/crypto'
 import { hashLink } from './hashLink'
-import { Action, EncryptedLink, Link, LinkBody, HashGraph } from './types'
+import { Action, EncryptedLink, Link, LinkBody, HashGraph, EncryptedHashGraph } from './types'
 import { KeysetWithSecrets } from '/keyset'
 
 /**
@@ -30,10 +30,10 @@ export const decryptLink = <A extends Action, C>(
  * implement their own version of `decryptChain` that reduces as it goes along, so that it can
  * determine the correct keyset to use for each link.  */
 export const decryptChain = <A extends Action, C>(
-  chain: HashGraph<A, C>,
+  chain: EncryptedHashGraph | HashGraph<A, C>,
   chainKeys: KeysetWithSecrets
 ): HashGraph<A, C> => {
-  const { encryptedLinks, links = {} } = chain
+  const { encryptedLinks, links = {} } = chain as HashGraph<A, C>
   const decryptedLinks = {} as Record<string, Link<A, C>>
 
   for (const hash in encryptedLinks) {
