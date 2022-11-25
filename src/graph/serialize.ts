8cdcdf601aff4cbd666b79e4a3857ec234654d76
HerbCaudill <herb@devresults.com>
2022-11-25 16:46:29 +0100

    graph/serialize.ts: use JSON

--- src/graph/serialize.ts
+++ src/graph/serialize.ts
@@ -1,28 +1,17 @@
-import { base58 } from '@herbcaudill/crypto'
 import { decryptGraph } from './decrypt'
-import { getChildMap } from './getParentMap'
-import { Action, EncryptedGraph, Graph, LinkMap } from './types'
-import { KeysetWithSecrets } from '/keyset'
-import msgpack from 'msgpack-lite'
+import { redactGraph } from './redactGraph'
+import { Action, Graph } from './types'
+import { Keyring, KeysetWithSecrets } from '/keyset'
 
 export const serialize = <A extends Action, C>(graph: Graph<A, C>) => {
-  // only persist the encrypted links
-  const { links, ...encryptedGraph } = graph
-
-  // to decrypt the graph, we'll need to know its dependency structure
-  const childMap = getChildMap(graph)
-
-  return base58.encode(msgpack.encode({ childMap, encryptedGraph }))
+  return JSON.stringify(redactGraph(graph))
 }
 
-export const deserialize = <A extends Action, C>(serialized: string, keys: KeysetWithSecrets): Graph<A, C> => {
-  const deserialized = msgpack.decode(base58.decode(serialized))
-
-  const { encryptedGraph, childMap } = deserialized as {
-    encryptedGraph: EncryptedGraph
-    childMap: LinkMap
-  }
+export const deserialize = <A extends Action, C>(
+  serialized: string,
+  keys: KeysetWithSecrets | Keyring
+): Graph<A, C> => {
+  const encryptedGraph = JSON.parse(serialized)
 
   return decryptGraph({ encryptedGraph, keys })
-  // return decryptGraph({ encryptedGraph, keys, childMap })
 }
